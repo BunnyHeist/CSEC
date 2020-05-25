b43520dd96bb975d153a445e37d1b5c4068c79ab
BunnyHeist <50196734+BunnyHeist@users.noreply.github.com>
2020-05-25 11:35:07 +0530

    Update Weird_Algorithm.py

--- Weird_Algorithm.py
+++ Weird_Algorithm.py
@@ -1,9 +1,15 @@
 n = int(input())
 
-while n!=1:
-    print(n)
-    if n%2==0:
-        n=n//2
-    else:
-        n=(n*3)+1
-print(1)
+print(n)
+
+while n!=1: # until n becomes 1 
+    
+    if n%2 == 0: 3 #if n is even
+       
+        n = n //2
+        print(n)
+    
+    else: # if n is odd
+       
+        n = n*3 +1
+        print(n)
